59cc5d9d38b052f0f079047103313e74bbb2127d
Krzysztof Stopa <k.stopa86@gmail.com>
2024-02-24 18:29:52 +0100

    feat: updated IP discovery script with region option

--- aws-asg-instances-ip.sh
+++ aws-asg-instances-ip.sh
@@ -2,10 +2,12 @@
 
 #@todo - command args for region and asg id
 
+# Set your region
+export REGION=us-east-2
 
-for ID in $(aws autoscaling describe-auto-scaling-instances --region us-west-1 --query AutoScalingInstances[].InstanceId --output text);
+for ID in $(aws autoscaling describe-auto-scaling-instances --region $REGION --query AutoScalingInstances[].InstanceId --output text);
 do
 
 
-aws ec2 describe-instances --instance-ids $ID --region us-west-1 --query Reservations[].Instances[].PublicIpAddress --output text
+aws ec2 describe-instances --instance-ids $ID --region $REGION --query Reservations[].Instances[].PublicIpAddress --output text
 done
